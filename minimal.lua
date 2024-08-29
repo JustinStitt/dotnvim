f0393097b1b4eeaa5c89c0d7e062f2410b8dd532
Justin Stitt <jstitt007@gmail.com>
2024-08-28 19:48:42 -0700

    Update minimal.lua
    fix clipboard issue with ssh and is 52nd using nvim v0.10+ features.

--- minimal.lua
+++ minimal.lua
@@ -1,11 +1,26 @@
 -- minimal nvim setup (no plugins, best for random servers)
 vim.opt.number = true
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("x", "<Space>", "<ESC>")
-vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
-vim.keymap.set("n", "<C-q>", "<cmd>:q<cr>")
-vim.api.nvim_set_option("clipboard", "unnamed")
-vim.keymap.set("n", "S", "/")
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
+vim.g.clipboard = {
+	name = 'OSC 52',
+	copy = {
+		['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+		['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+	},
+	paste = {
+		['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+		['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+	},
+}
+vim.opt.clipboard="unnamedplus"
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("x", "<Space>", "<ESC>")
+vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
+vim.keymap.set("n", "<Esc>", "<cmd>:noh<cr>")
+vim.keymap.set("i", "<C-s>", "<cmd>:w<cr><Esc>")
+vim.keymap.set("n", "<C-q>", "<cmd>:q<cr>")
+vim.keymap.set({ "x", "v" }, "J", " :m '>+<cr>gv=gv<left>")
+vim.keymap.set({ "x", "v" }, "K", " :m '<-2<CR>gv=gv<left>", { noremap = true })
+vim.keymap.set("n", "S", "/")
