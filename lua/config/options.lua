2a90ec0356d522593a4f9436eea5c201107e2bfa
Justin Stitt <justinstitt@google.com>
2026-03-20 09:54:51 -0700

    update clipboard, remove obsidian
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,6 @@ vim.opt.relativenumber = false
 vim.opt.cursorline = true
 vim.opt.statuscolumn = ""
 vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 
 vim.g.mkdp_port = "56777"
 vim.g.mkdp_echo_preview_url = 1
@@ -14,6 +13,21 @@ vim.g.mkdp_echo_preview_url = 1
 vim.cmd([[ set background=dark ]])
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.snacks_animate = false -- because i want to use neoscroll
+
+-- clipboard over ssh with byobu/tmux
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
+vim.opt.clipboard = "unnamedplus"
 
 -- LSP Server to use for Python (from https://www.lazyvim.org/extras/lang/python)
 -- vim.g.lazyvim_python_lsp = "basedpyright"
