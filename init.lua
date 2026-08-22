19196631c82684fc384ad85e0229e920017b64cd
Justin Stitt <jstitt007@gmail.com>
2026-08-21 21:29:24 -0700

    add askpass stuff, idk if it works tho
    Signed-off-by: Justin Stitt <jstitt007@gmail.com>

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
+-- Point sudo/ssh/git at the askpass helper regardless of how nvim was launched
+-- (a GUI launcher never sources .zshrc, and /etc/sudo.conf still defaults to
+-- ssh-askpass, which can't open a display under niri). See ~/.local/bin/askpass.
+local askpass = vim.fn.expand("~/.local/bin/askpass")
+if vim.fn.executable(askpass) == 1 then
+  vim.env.SUDO_ASKPASS = askpass
+  vim.env.SSH_ASKPASS = askpass
+  vim.env.GIT_ASKPASS = askpass
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
